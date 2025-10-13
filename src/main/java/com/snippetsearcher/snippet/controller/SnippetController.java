9ef6d329ed433698514329b89eadeb62ea75d2d9
taism <tmmarambiocapelli@mail.austral.edu.ar>
2025-10-13 12:26:15 -0300

    add SnippetController with REST annotations for snippet endpoints

--- src/main/java/com/snippetsearcher/snippet/controller/SnippetController.java
+++ src/main/java/com/snippetsearcher/snippet/controller/SnippetController.java
@@ -1,4 +1,9 @@
 package com.snippetsearcher.snippet.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/snippets")
 public class SnippetController {
 }
