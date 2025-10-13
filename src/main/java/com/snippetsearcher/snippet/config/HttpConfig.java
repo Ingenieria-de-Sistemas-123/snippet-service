a0cafcc1e8fa37f533eafdd8dee0e1e3ef67eac4
taism <tmmarambiocapelli@mail.austral.edu.ar>
2025-10-13 01:02:53 -0300

    add HttpConfig class for RestClient configuration

---
+++ src/main/java/com/snippetsearcher/snippet/config/HttpConfig.java
@@ -0,0 +1,14 @@
+package com.snippetsearcher.snippet.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+
+@org.springframework.context.annotation.Configuration
+public class HttpConfig {
+    @Bean
+    RestClient restClient(RestClient.Builder b,
+                          @Value("${language.base-url}") String baseUrl){
+        return b.baseUrl(baseUrl).build();
+    }
+}
