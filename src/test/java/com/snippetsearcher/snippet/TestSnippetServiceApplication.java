70adf4d5e2954e8b7249fef5a020280b77454a6c
taism <tmmarambiocapelli@mail.austral.edu.ar>
2025-10-12 23:23:51 -0300

    initialize snippet service with Spring Boot and PostgreSQL support

---
+++ src/test/java/com/snippetsearcher/snippet/TestSnippetServiceApplication.java
@@ -0,0 +1,11 @@
+package com.snippetsearcher.snippet;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestSnippetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(SnippetServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
