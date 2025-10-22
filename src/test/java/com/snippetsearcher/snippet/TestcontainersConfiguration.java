9d8aa3a68213d950f79b4dc0df7bf0a032f17813
Abrojito <abrilbrieva@gmail.com>
2025-10-22 18:07:37 -0300

    added application-test.yml for testing environment

--- src/test/java/com/snippetsearcher/snippet/TestcontainersConfiguration.java
+++ src/test/java/com/snippetsearcher/snippet/TestcontainersConfiguration.java
@@ -12,7 +12,9 @@ class TestcontainersConfiguration {
 	@Bean
 	@ServiceConnection
 	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"))
+				.withDatabaseName("test")
+				.withUsername("test")
+				.withPassword("test");
 	}
-
 }
