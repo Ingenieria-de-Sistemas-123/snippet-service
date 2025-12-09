60320c8a77084121757ddbc449bcdb5d5f092b76
TotoBerger <tberger@mail.austral.edu.ar>
2025-12-09 13:56:50 -0300

    fix

--- src/main/java/com/snippetsearcher/snippet/config/RedisConfig.java
+++ src/main/java/com/snippetsearcher/snippet/config/RedisConfig.java
@@ -1,17 +1,21 @@
 package com.snippetsearcher.snippet.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
   @Bean
-  public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory();
+  public RedisConnectionFactory redisConnectionFactory(
+          @Value("${spring.data.redis.host}") String host,
+          @Value("${spring.data.redis.port}") int port
+  ) {
+    return new LettuceConnectionFactory(host, port);
   }
 
   @Bean
